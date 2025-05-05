2050dcd60eb2fb229c00ba27c28bc2353a7c6f16
dims3926r <oleksijmirov@gmail.com>
2025-05-05 14:21:13 +0300

    Update task.c

--- p2/p2.1/task.c
+++ p2/p2.1/task.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t example_time;
+    size_t time_size = sizeof(example_time);
+
+    printf("Тип time_t займає %zu байт(и)\n", time_size);
+
+    if (time_size == 4) {
+        time_t limit_time = 0x7FFFFFFF;
+        printf("На 32-бітній архітектурі час досягне межі: %s", ctime(&limit_time));
+    } else {
+        printf("На 64-бітних системах обмеження часу практично недосяжне\n");
+    }
+
+    return 0;
+}
 
