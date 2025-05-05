c6ad21e2d680942861dac3c688baaf92497d059a
dims3926r <oleksijmirov@gmail.com>
2025-05-05 15:44:18 +0300

    Update task.c

--- p2/p11/task.c
+++ p2/p11/task.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int global_var = 42;
+
+void check_segments() {
+    int local_var = 10;
+    static int static_var = 20;
+    void *heap_var = malloc(1);
+
+    printf("=== Перевірка розташування сегментів ===\n");
+    printf("Адреса функції (text segment):      %p\n", (void*)check_segments);
+    printf("Адреса глобальної змінної:          %p\n", (void*)&global_var);
+    printf("Адреса статичної змінної:           %p\n", (void*)&static_var);
+    printf("Адреса локальної змінної (stack):   %p\n", (void*)&local_var);
+    printf("Адреса динамічної змінної (heap):   %p\n", heap_var);
+
+    free(heap_var);
+}
+
+int main() {
+    check_segments();
+    return 0;
+}
 
