4203f29a44fa48c7f2a5fac9228c08a1e596de65
dims3926r <oleksijmirov@gmail.com>
2025-05-17 14:50:05 +0300

    Update task.c

--- p7/p7.10/task.c
+++ p7/p7.10/task.c
@@ -5,7 +5,6 @@
 int main(void) {
     srand((unsigned int)time(NULL));
 
-    // Випадкове число від 0.0 до 1.0
     double r1 = (double)rand() / RAND_MAX;
     printf("Від 0.0 до 1.0: %.6f\n", r1);
 
@@ -16,7 +15,6 @@ int main(void) {
         return 1;
     }
 
-    // Випадкове число від 0.0 до n
     double r2 = ((double)rand() / RAND_MAX) * n;
     printf("Від 0.0 до %.6f: %.6f\n", n, r2);
 
